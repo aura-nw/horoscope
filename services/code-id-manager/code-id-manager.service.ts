ddbd36f44f9340f95bf6c4889d81dbf8193e73e6
Phan Anh Tuan <fibonacci998@gmail.com>
2022-07-06 15:34:21 +0700

    add version for code id manager

--- services/code-id-manager/code-id-manager.service.ts
+++ services/code-id-manager/code-id-manager.service.ts
@@ -8,12 +8,11 @@ import { Ok } from 'ts-results';
 
 @Service({
 	name: 'code_id',
-	mixins: [
-		dbCodeIDMixin,
-	],
+	mixins: [dbCodeIDMixin],
+	version: 1,
 	actions: {
-		"checkStatus": {
-			async handler(ctx: Context){
+		checkStatus: {
+			async handler(ctx: Context) {
 				// @ts-ignore
 				let foundCodeID = await this.adapter.findOne(ctx.params);
 				// @ts-ignore
@@ -23,18 +22,16 @@ import { Ok } from 'ts-results';
 				} else {
 					return Ok;
 				}
-			}
+			},
 		},
-		"updateMany": {
+		updateMany: {
 			async handler(ctx: Context): Promise<any> {
 				// @ts-ignore
-				this.logger.debug(`ctx.params ${ctx.params.condition, ctx.params.update}`);
+				this.logger.debug(`ctx.params ${(ctx.params.condition, ctx.params.update)}`);
 				// @ts-ignore
-				return await this.adapter.updateMany(ctx.params.condition,ctx.params.update);
-			}
-		}
+				return await this.adapter.updateMany(ctx.params.condition, ctx.params.update);
+			},
+		},
 	},
 })
-
-export default class CodeIDService extends moleculer.Service {
-}
+export default class CodeIDService extends moleculer.Service {}
