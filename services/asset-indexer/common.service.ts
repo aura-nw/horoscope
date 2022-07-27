368d86634b5929ef5f236e5fa0c2c3d395e2aac5
ThienLK <koankem0901@gmail.com>
2022-07-27 14:37:03 +0700

    init

--- services/asset-indexer/common.service.ts
+++ services/asset-indexer/common.service.ts
@@ -1,17 +1,19 @@
-/* eslint-disable @typescript-eslint/explicit-member-accessibility */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 'use strict';
 
 import CallApiMixin from "@Mixins/callApi/call-api.mixin";
 // import http, { request } from "http";
 import request from "request-promise";
 // import fs from "fs";
-// import * as IPFS from 'ipfs-core';
 // import parse from "url-parse";
 // import Url from "url-parse";
 // import File from "typescript";
 import CID from 'cids';
+const fetch = require("node-fetch");
+// const fileTypeFromBuffer = require ("file-type");
+// import fileTypeFromFile from 'file-type';
 // import fileTypeFromBuffer, { fileType, FileTypeResult } from "file-type";
+// import fileTypeFromFile from "file-type";
+// import * as FileType from "file-type";
 import parse from "parse-uri";
 import { Config } from "common";
 import { Types } from "mongoose";
@@ -20,6 +22,7 @@ import { Types } from "mongoose";
 import { Action, Service } from '@ourparentcenter/moleculer-decorators-extended';
 import moleculer, { Context } from "moleculer";
 import { S3Service } from '../../utils/s3';
+// import { IPFS } from '../../utils/ipfs';
 
 const CODE_ID_URI = Config.CODE_ID_URI;
 const CONTRACT_URI_LIMIT = Config.ASSET_INDEXER_CONTRACT_URI_LIMIT;
@@ -93,6 +96,31 @@ export default class CommonService extends moleculer.Service {
         } while (next_key != null);
         return contractList;
     }
+    
+    private async getFile(ctx: Context<TokenInfo>) {
+        // const URL = ctx.params.URL;
+        // const code_id = ctx.params.code_id;
+        // let contractList: any[] = [];
+        // const urlGetContractList = `${CODE_ID_URI}${code_id}/contracts?pagination.limit=${CONTRACT_URI_LIMIT}&pagination.countTotal=true&`;
+        // let path = `${CODE_ID_URI}${code_id}/contracts?pagination.limit=${CONTRACT_URI_LIMIT}&pagination.countTotal=true&`;
+        // let next_key = null;
+        // do {
+            // @ts-ignore
+            let resultCallApi = await this.callApiFromDomain("ipfs.io/", "api/v0/file/ls?arg=QmUpd48SunBdGRgYEdCMgwDY4NdnSTmt5UnNcEqbDytZWt");
+            this.logger.info('Call resultCallApi return ', resultCallApi);
+            // if (resultCallApi?.contracts?.length > 0) {
+            //     contractList.push(...resultCallApi.contracts);
+            //     next_key = resultCallApi.pagination.next_key;
+            //     if (next_key === null) {
+            //         break;
+            //     }
+            //     path = `${urlGetContractList}pagination.key=${encodeURIComponent(next_key)}`;
+            // } else {
+            //     // @ts-ignore
+            // }
+        // } while (next_key != null);
+        return "";
+    }
 }
 
 export class Common {
@@ -120,6 +148,11 @@ export class Common {
             history: [],
         };
     }
+    // public static loadIpfs = async function () {
+    //     // const { create } = await import('ipfs-core');
+    //     const node = await IPFS.create();
+    //     return node
+    // }
     public static getFileFromUrl = async function (url: string, name: string, defaultType = 'image/jpeg') {
         // const response = await fetch(url);
         // const data = await response.blob();
@@ -159,12 +192,12 @@ export class Common {
         const urlx = "ipfs://QmUpd48SunBdGRgYEdCMgwDY4NdnSTmt5UnNcEqbDytZWt/104"
         let parsed = parse(urlx);
         console.log(parsed);
-        const ipfs = await IPFS.create()
         const cidBase32 = new CID(parsed.host).toV1().toString('base32');
-        console.log(ipfs.ls(cidBase32));
+        // const ipfs = await Common.loadIpfs();
+        // console.log(ipfs.ls(cidBase32));
         // const uri = `https://dweb.link/ipfs/${cidBase32}`;
-        const uri = `https://dweb.link/ipfs/bafybeifpmrt4vd3mtm3y6tqg5kkr3xmn2e3tyml3c6i4urconai3esrhty`;
-        console.log(`dweb.link/ipfs/${cidBase32}`);
+        const uri = `https://dweb.link/ipfs/bafybeidakcbv2uxivbtrf53nabui6bcw2mdkbuwwlyr4qfev7cid3irtxu`;
+        console.log(`https://dweb.link/ipfs/${cidBase32}`);
         var options = {
             uri,
             // uri: url,
@@ -186,17 +219,24 @@ export class Common {
         // console.log(URL);
         // console.log(URL.protocol=="ipfs:");
         // console.log(URL.host);
+        // var req = await fetch(uri, { method: 'HEAD' });
+        // console.log(req.headers);
+        // console.log(req.headers.get('content-type'));
         request(options, async function (error: any, response: { statusCode: number; }, body: any) {
             console.log("request get image");
             if (error || response.statusCode !== 200) {
                 console.log("failed to get image");
                 console.log(error);
             } else {
-                // let rs = await fileTypeFromBuffer(body);
+                console.log("request get image ok");
+                // let rs = await fileTypeFromFile(body);
                 // console.log( fileTypeFromBuffer(body));
                 // const { ext, mime } = await FileType.fromBuffer(body) as FileTypeResult;
 
                 // var ext = fileName.substr(fileName.lastIndexOf('.') + 1);
+                // const type = await fileTypeFromFile(body);
+                // const type = await FileType.fileTypeFromFile(body);
+                // const node = await IPFS.connectIPFS();
                 s3Client.putObject({
                     Body: body,
                     Key: "e1.png",
