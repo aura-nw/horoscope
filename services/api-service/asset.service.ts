65aad667f084da5f24d6398c4f8c0a1b318d864f
Phan Anh Tuan <fibonacci998@gmail.com>
2022-07-29 14:58:40 +0700

    update swagger asset

--- services/api-service/asset.service.ts
+++ services/api-service/asset.service.ts
@@ -195,6 +195,7 @@ export default class BlockService extends MoleculerDBService<
 	 *        - in: query
 	 *          name: chainid
 	 *          required: false
+	 *          enum: ["aura-testnet","serenity-testnet-001","halo-testnet-001","theta-testnet-001","osmo-test-4","evmos_9000-4","euphoria-1"]
 	 *          type: string
 	 *          description: "Chain Id of network need to query(if null it will return asset on all chainid)"
 	 *        - in: query
@@ -308,6 +309,7 @@ export default class BlockService extends MoleculerDBService<
 	 *          name: chainid
 	 *          required: false
 	 *          type: string
+	 *          enum: ["aura-testnet","serenity-testnet-001","halo-testnet-001","theta-testnet-001","osmo-test-4","evmos_9000-4","euphoria-1"]
 	 *          description: "Chain Id of network need to query"
 	 *        - in: query
 	 *          name: pageLimit
