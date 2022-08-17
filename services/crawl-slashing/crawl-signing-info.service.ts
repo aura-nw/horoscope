6f1fbcf67b69e1116a9078b2c7cf5c9e1a785a60
Phan Anh Tuan <fibonacci998@gmail.com>
2022-08-17 10:41:13 +0700

    revert crawl signing info

--- services/crawl-slashing/crawl-signing-info.service.ts
+++ services/crawl-slashing/crawl-signing-info.service.ts
@@ -68,69 +68,61 @@ export default class CrawlSigningInfoService extends Service {
 		const prefixAddress = LIST_NETWORK.find(
 			(item) => item.chainId === Config.CHAIN_ID,
 		)?.prefixAddress;
-		let listBulk: any[] = await Promise.all(
-			listFoundValidator.map(async (foundValidator: ValidatorEntity) => {
-				try {
-					let consensusPubkey = foundValidator.consensus_pubkey;
-					this.logger.debug(
-						`Found validator with address ${foundValidator.operator_address}`,
-					);
-					this.logger.debug(`Found validator with consensusPubkey ${consensusPubkey}`);
+		listFoundValidator.map(async (foundValidator: ValidatorEntity) => {
+			try {
+				let consensusPubkey = foundValidator.consensus_pubkey;
+				this.logger.debug(
+					`Found validator with address ${foundValidator.operator_address}`,
+				);
+				this.logger.debug(`Found validator with consensusPubkey ${consensusPubkey}`);
 
-					const pubkey = this.getAddressHexFromPubkey(consensusPubkey.key.toString());
-					const consensusAddress = this.hexToBech32(
-						pubkey,
-						`${prefixAddress}${Config.CONSENSUS_PREFIX_ADDRESS}`,
-					);
-					let path = `${Config.GET_SIGNING_INFO}/${consensusAddress}`;
+				const pubkey = this.getAddressHexFromPubkey(consensusPubkey.key.toString());
+				const consensusAddress = this.hexToBech32(
+					pubkey,
+					`${prefixAddress}${Config.CONSENSUS_PREFIX_ADDRESS}`,
+				);
+				let path = `${Config.GET_SIGNING_INFO}/${consensusAddress}`;
 
-					this.logger.info(path);
-					let result: ISigningInfoEntityResponseFromLCD = await this.callApiFromDomain(
-						url,
-						path,
-					);
-					this.logger.debug(result);
+				this.logger.info(path);
+				let result: ISigningInfoEntityResponseFromLCD = await this.callApiFromDomain(
+					url,
+					path,
+				);
+				this.logger.debug(result);
 
-					if (result.val_signing_info) {
-						let paramSlashing: ParamEntity[] = await this.broker.call(
-							'v1.crawlparam.find',
-							{
-								query: {
-									'custom_info.chain_id': Config.CHAIN_ID,
-									module: MODULE_PARAM.SLASHING,
-								},
+				if (result.val_signing_info) {
+					let paramSlashing: ParamEntity[] = await this.broker.call(
+						'v1.crawlparam.find',
+						{
+							query: {
+								'custom_info.chain_id': Config.CHAIN_ID,
+								module: MODULE_PARAM.SLASHING,
 							},
-						);
-						let uptime: Number = 0;
-						if (paramSlashing.length > 0) {
-							const blockWindow =
-								//@ts-ignore
-								paramSlashing[0].params?.signed_blocks_window.toString();
-							const missedBlock =
-								result.val_signing_info.missed_blocks_counter.toString();
-							uptime =
-								Number(
-									((BigInt(blockWindow) - BigInt(missedBlock)) *
-										BigInt(100000000)) /
-										BigInt(blockWindow),
-								) / 1000000;
-						}
-						// let res = await this.adapter.updateById(foundValidator._id, {
-						// 	$set: { val_signing_info: result.val_signing_info, uptime: uptime },
-						// });
-						// this.logger.debug(res);
-						foundValidator.val_signing_info = result.val_signing_info;
-						foundValidator.uptime = uptime;
-						return foundValidator;
+						},
+					);
+					let uptime: Number = 0;
+					if (paramSlashing.length > 0) {
+						//@ts-ignore
+						const blockWindow = paramSlashing[0].params.signed_blocks_window.toString();
+						const missedBlock =
+							result.val_signing_info.missed_blocks_counter.toString();
+						uptime =
+							Number(
+								((BigInt(blockWindow) - BigInt(missedBlock)) * BigInt(100000000)) /
+									BigInt(blockWindow),
+							) / 1000000;
 					}
-				} catch (error) {
-					this.logger.error(error);
+					let res = await this.adapter.updateById(foundValidator._id, {
+						$set: { val_signing_info: result.val_signing_info, uptime: uptime },
+					});
+					this.logger.debug(res);
 				}
-				return;
-			}),
-		);
-		let result = await this.adapter.bulkWrite(listBulk);
-		this.logger.info(`result : ${listBulk.length}`, result);
+			} catch (error) {
+				this.logger.error(error);
+			}
+		});
+		// let result = await this.adapter.bulkWrite(listBulk);
+		// this.logger.info(`result : ${listBulk.length}`, result);
 	}
 	getAddressHexFromPubkey(pubkey: string) {
 		var bytes = Buffer.from(pubkey, 'base64');
