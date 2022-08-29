10f4141390598229789e068c8d8d95cc0fe39fce
Phan Anh Tuan <fibonacci998@gmail.com>
2022-08-29 16:36:57 +0700

    update power event api

--- services/api-service/transaction.service.ts
+++ services/api-service/transaction.service.ts
@@ -27,7 +27,7 @@ import { toBase64, toUtf8 } from '@cosmjs/encoding';
 import { Utils } from '../../utils/utils';
 import { callApiMixin } from '../../mixins/callApi/call-api.mixin';
 import { Config } from '../../common';
-import { flatten } from 'lodash';
+import { add, flatten } from 'lodash';
 /**
  * @typedef {import('moleculer').Context} Context Moleculer's Context
  */
@@ -632,59 +632,68 @@ export default class BlockService extends MoleculerDBService<
 		query['custom_info.chain_id'] = ctx.params.chainid;
 
 		let listQueryAnd: any[] = [];
-
+		let listQueryOr: any[] = [];
 		if (address) {
-			listQueryAnd.push({
-				$or: [
-					{
-						'tx_response.events': {
-							$elemMatch: {
-								type: 'delegate',
-								'attributes.key': BASE_64_ENCODE.VALIDATOR,
-								'attributes.value': toBase64(toUtf8(address)),
-							},
-						},
-					},
-					{
-						'tx_response.events': {
-							$elemMatch: {
-								type: 'redelegate',
-								'attributes.key': BASE_64_ENCODE.SOURCE_VALIDATOR,
-								'attributes.value': toBase64(toUtf8(address)),
-							},
-						},
-					},
-					{
-						'tx_response.events': {
-							$elemMatch: {
-								type: 'redelegate',
-								'attributes.key': BASE_64_ENCODE.DESTINATION_VALIDATOR,
-								'attributes.value': toBase64(toUtf8(address)),
-							},
-						},
-					},
-					{
-						'tx_response.events': {
-							$elemMatch: {
-								type: 'unbond',
-								'attributes.key': BASE_64_ENCODE.VALIDATOR,
-								'attributes.value': toBase64(toUtf8(address)),
-							},
-						},
-					},
-				],
-			});
+			// listQueryAnd.push({
+			// 	$or: [
+			// 		{
+			// 			'tx_response.events': {
+			// 				$elemMatch: {
+			// 					type: 'delegate',
+			// 					'attributes.key': BASE_64_ENCODE.VALIDATOR,
+			// 					'attributes.value': toBase64(toUtf8(address)),
+			// 				},
+			// 			},
+			// 		},
+			// 		{
+			// 			'tx_response.events': {
+			// 				$elemMatch: {
+			// 					type: 'redelegate',
+			// 					'attributes.key': BASE_64_ENCODE.SOURCE_VALIDATOR,
+			// 					'attributes.value': toBase64(toUtf8(address)),
+			// 				},
+			// 			},
+			// 		},
+			// 		{
+			// 			'tx_response.events': {
+			// 				$elemMatch: {
+			// 					type: 'redelegate',
+			// 					'attributes.key': BASE_64_ENCODE.DESTINATION_VALIDATOR,
+			// 					'attributes.value': toBase64(toUtf8(address)),
+			// 				},
+			// 			},
+			// 		},
+			// 		{
+			// 			'tx_response.events': {
+			// 				$elemMatch: {
+			// 					type: 'unbond',
+			// 					'attributes.key': BASE_64_ENCODE.VALIDATOR,
+			// 					'attributes.value': toBase64(toUtf8(address)),
+			// 				},
+			// 			},
+			// 		},
+			// 	],
+			// });
+			listQueryOr.push(
+				{ 'indexes.delegate_validator': address },
+				{ 'indexes.redelegate_source_validator': address },
+				{ 'indexes.redelegate_destination_validator': address },
+				{ 'indexes.unbond_validator': address },
+			);
 		}
 
 		if (listQueryAnd.length > 0) {
 			query['$and'] = listQueryAnd;
+		}
+		if (listQueryOr.length > 0) {
+			query['$or'] = listQueryOr;
 		}
 
 		this.logger.info('query: ', JSON.stringify(query));
 		let listPromise = [];
 
 		listPromise.push(
-			this.adapter.find({
+			this.adapter.lean({
 				query: query,
 				// @ts-ignore
 				sort: sort,
@@ -709,6 +718,11 @@ export default class BlockService extends MoleculerDBService<
 
 			let nextKey = null;
 			if (result.length > 0) {
+				await result.forEach((tx: any) => {
+					delete tx['custom_info'];
+					delete tx['indexes'];
+				});
+
 				if (result.length == 1) {
 					nextKey = result[result.length - 1]?._id;
 				} else {
