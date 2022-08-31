11ca166e4ff72d25188ebd1fdd3ee96b827b7ed6
duck59uet <ducnv1911@gmail.com>
2022-08-31 17:05:55 +0700

    Handle event crawl block

--- services/websocket/websocket.service.ts
+++ services/websocket/websocket.service.ts
@@ -3,83 +3,134 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import moleculer, { Context } from 'moleculer';
+import { Context, CallingOptions, Service, ServiceBroker } from 'moleculer';
 import SocketIOMixin from 'moleculer-io';
-import { Action, Service } from '@ourparentcenter/moleculer-decorators-extended';
 import ApiGatewayService from 'moleculer-web';
 import { Config } from '../../common';
-import { createAdapter } from 'socket.io-redis';
 import { Job } from 'bull';
 import { ListTxCreatedParams, TransactionArrayParam } from 'types';
-const QueueService = require('moleculer-bull');
 import RedisMixin from '../../mixins/redis/redis.mixin';
 import { RedisClientType } from 'redis';
+import { ITransaction } from 'entities';
+const QueueService = require('moleculer-bull');
 
 /**
  * @typedef {import('moleculer').Context} Context Moleculer's Context
  * @typedef {import('http').IncomingMessage} IncomingRequest Incoming HTTP Request
  * @typedef {import('http').ServerResponse} ServerResponse HTTP Server Response
  */
-const redisAdapter = createAdapter(
-	`redis://${Config.REDIS_USERNAME}:${Config.REDIS_PASSWORD}@${Config.REDIS_HOST}:${Config.REDIS_PORT}/${Config.REDIS_DB_NUMBER}`,
-);
-const redisMixin = new RedisMixin().start();
-@Service({
-	name: 'io',
-	broker: {},
-	//@ts-ignore
-	mixins: [
-		ApiGatewayService,
-		SocketIOMixin,
-		redisMixin,
-		QueueService(
-			`redis://${Config.REDIS_USERNAME}:${Config.REDIS_PASSWORD}@${Config.REDIS_HOST}:${Config.REDIS_PORT}/${Config.REDIS_DB_NUMBER}`,
-			{
-				prefix: 'websocket.tx-handle',
-			},
-		),
-	],
-	queues: {
-		'websocket.tx-handle': {
-			process(job: Job) {
-				// @ts-ignore
-				this.handleJob(URL, job.data.listTx, job.data.chainId);
 
-				return true;
+const MAX_RETRY_REQ = Config.ASSET_INDEXER_MAX_RETRY_REQ;
+const ACTION_TIMEOUT = Config.ASSET_INDEXER_ACTION_TIMEOUT;
+const OPTs: CallingOptions = { timeout: ACTION_TIMEOUT, retries: MAX_RETRY_REQ };
+
+export default class WebsocketService extends Service {
+
+	private redisMixin = new RedisMixin().start();
+
+	public constructor(public broker: ServiceBroker) {
+		super(broker);
+		this.parseServiceSchema({
+			name: 'io',
+			version: 1,
+			mixins: [
+				QueueService(
+					`redis://${Config.REDIS_USERNAME}:${Config.REDIS_PASSWORD}@${Config.REDIS_HOST}:${Config.REDIS_PORT}/${Config.REDIS_DB_NUMBER}`,
+					{
+						prefix: 'websocket.tx-handle',
+					},
+				),
+				ApiGatewayService,
+				SocketIOMixin,
+				this.redisMixin,
+			],
+			queues: {
+				'websocket.tx-handle': {
+					process(job: Job) {
+						// @ts-ignore
+						this.handleNewBlock(job.data.listTx, job.data.chainId);
+
+						return true;
+					},
+				},
 			},
-		},
-	},
-	// More info about settings: https://moleculer.services/docs/0.14/moleculer-web.html
-	settings: {
-		port: 3000,
-		io: {
-			options: {}, //socket.io options
-			namespaces: {
-				'/register': {
-					events: {
-						call: {
-							whitelist: ['io.clientRegister'],
+			events: {
+				'list-tx.upsert': {
+					handler: (ctx: any) => {
+						this.createJob(
+							'websocket.tx-handle',
+							{
+								listTx: ctx.params.listTx,
+								chainId: ctx.params.chainId,
+							},
+							{
+								removeOnComplete: true,
+							},
+						);
+						return;
+					},
+				},
+			},
+			settings: {
+				port: 3000,
+				io: {
+					options: {}, //socket.io options
+					namespaces: {
+						'/register': {
+							events: {
+								'call': {
+									whitelist: ['v1.io.client-register'],
+								}
+							},
 						},
 					},
 				},
 			},
-		},
-	},
-	events: {
-		//Subcribe to 'list-tx.upsert' events
-		'list-tx.upsert': {
-			handler: async (ctx: Context<ListTxCreatedParams>) => {},
-		},
-	},
-})
-export default class WebsocketService extends moleculer.Service {
-	@Action()
-	async clientRegister(ctx: Context<TransactionArrayParam>) {
-		let redisClient: RedisClientType = await this.getRedisClient();
-		ctx.params.txHashArr.forEach((tx) => {
-			redisClient.HSET('myhash', 'txHash', tx);
+			actions: {
+				'client-register': {
+					async handler(ctx: Context<TransactionArrayParam>) {	
+						// @ts-ignore
+						await this.clientRegister(ctx);
+					},
+				},
+			}
 		});
 	}
+	//@ts-ignore
+	// More info about settings: https://moleculer.services/docs/0.14/moleculer-web.html
+	async clientRegister(ctx: Context<TransactionArrayParam>) {
+		let redisClient: RedisClientType = await this.getRedisClient();
+		let result = await redisClient.SADD("hash", ctx.params.txHashArr);
+		this.logger.info(result);
+	}
 
-	async txFilter(ctx: Context<ListTxCreatedParams>) {}
+	async txFilter(ctx: Context<ListTxCreatedParams>) { }
+
+	async handleNewBlock(listTx: ITransaction[], chainId: string): Promise<any[]> {
+		try {
+			let redisClient: RedisClientType = await this.getRedisClient();
+			this.logger.info('listTx', listTx);
+			let blockTxHash = listTx[0].tx_response.txhash;
+			let txHashChecked = await redisClient.SISMEMBER('myhash', blockTxHash.toString());
+
+			this.logger.info('Check', txHashChecked);
+
+		} catch (error) {
+			this.logger.error(error);
+		}
+		return [];
+	}
+
+	async _start() {
+		this.getQueue('websocket.tx-handle').on('completed', (job: Job) => {
+			this.logger.debug(`Job #${job.id} completed!. Result:`, job.returnvalue);
+		});
+		this.getQueue('websocket.tx-handle').on('failed', (job: Job) => {
+			this.logger.error(`Job #${job.id} failed!. Result:`, job.stacktrace);
+		});
+		this.getQueue('websocket.tx-handle').on('progress', (job: Job) => {
+			this.logger.debug(`Job #${job.id} progress is ${job.progress()}%`);
+		});
+		return super._start();
+	}
 }
