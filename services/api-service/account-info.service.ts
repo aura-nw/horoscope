7e08f04f234dda46e72e08e4620b64d785db868e
doquockhanhan <doquockhanhan@gmail.com>
2022-10-19 14:13:22 +0700

    catch broker call handle address service

--- services/api-service/account-info.service.ts
+++ services/api-service/account-info.service.ts
@@ -319,15 +319,13 @@ export default class AccountInfoService extends MoleculerDBService<
 		]);
 
 		if (accountInfo) {
-			try {
-				this.broker.call('v1.handleAddress.accountinfoupsert', {
-					listTx: [{ address: ctx.params.address, message: '' }],
-					source: CONST_CHAR.API,
-					chainId: ctx.params.chainId,
-				});
-			} catch (error) {
+			this.broker.call('v1.handleAddress.accountinfoupsert', {
+				listTx: [{ address: ctx.params.address, message: '' }],
+				source: CONST_CHAR.API,
+				chainId: ctx.params.chainId,
+			}).catch((error) => {
 				this.logger.error(error);
-			}
+			});
 			accountInfo = accountInfo.toObject();
 			accountInfo.account_delegate_rewards = accountRewards;
 			const data = accountInfo;
@@ -339,15 +337,13 @@ export default class AccountInfoService extends MoleculerDBService<
 			this.logger.info(JSON.stringify(result));
 			return result;
 		} else {
-			try {
-				this.broker.call('v1.handleAddress.accountinfoupsert', {
-					listTx: [{ address: ctx.params.address, message: '' }],
-					source: CONST_CHAR.API,
-					chainId: ctx.params.chainId,
-				});
-			} catch (error) {
+			this.broker.call('v1.handleAddress.accountinfoupsert', {
+				listTx: [{ address: ctx.params.address, message: '' }],
+				source: CONST_CHAR.API,
+				chainId: ctx.params.chainId,
+			}).catch((error) => {
 				this.logger.error(error);
-			}
+			});
 			if (!accountRewards.code) {
 				const result: ResponseDto = {
 					code: ErrorCode.SUCCESSFUL,
@@ -544,15 +540,13 @@ export default class AccountInfoService extends MoleculerDBService<
 		]);
 		let result: ResponseDto;
 		if (accountInfo.length > 0) {
-			try {
-				this.broker.call('v1.handleAddress.accountinfoupsert', {
-					listTx: [{ address: ctx.params.address, message: '' }],
-					source: CONST_CHAR.API,
-					chainId: ctx.params.chainId,
-				});
-			} catch (error) {
+			this.broker.call('v1.handleAddress.accountinfoupsert', {
+				listTx: [{ address: ctx.params.address, message: '' }],
+				source: CONST_CHAR.API,
+				chainId: ctx.params.chainId,
+			}).catch((error) => {
 				this.logger.error(error);
-			}
+			});
 			let data = Object.assign({}, accountInfo[0]);
 			data.account_delegate_rewards = accountRewards;
 			result = {
@@ -561,15 +555,13 @@ export default class AccountInfoService extends MoleculerDBService<
 				data,
 			};
 		} else {
-			try {
-				this.broker.call('v1.handleAddress.accountinfoupsert', {
-					listTx: [{ address: ctx.params.address, message: '' }],
-					source: CONST_CHAR.API,
-					chainId: ctx.params.chainId,
-				});
-			} catch (error) {
+			this.broker.call('v1.handleAddress.accountinfoupsert', {
+				listTx: [{ address: ctx.params.address, message: '' }],
+				source: CONST_CHAR.API,
+				chainId: ctx.params.chainId,
+			}).catch((error) => {
 				this.logger.error(error);
-			}
+			});
 			if (!accountRewards.code) {
 				const result: ResponseDto = {
 					code: ErrorCode.SUCCESSFUL,
