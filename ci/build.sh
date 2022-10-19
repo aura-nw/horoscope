141f309763d6530412212d57242d28883657b1dc
Phan Anh Tuan <fibonacci998@gmail.com>
2022-10-19 13:42:13 +0700

    update build ci script

--- ci/build.sh
+++ ci/build.sh
@@ -4,5 +4,5 @@ set -xe
 #Login to registry
 echo $GITHUB_PASSWORD | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
 #Build and push image
-docker build -t ghcr.io/aura-nw/aura-indexer-crawl:feature-multiple-db_$(git rev-parse --short HEAD) -f Dockerfile .
-docker push ghcr.io/aura-nw/aura-indexer-crawl:feature-multiple-db_$(git rev-parse --short HEAD)
+docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
+docker push ${CONTAINER_RELEASE_IMAGE}
