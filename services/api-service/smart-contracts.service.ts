f6b6161c7ff9f6b7c51d85a031005198971e3c70
Phan Anh Tuan <fibonacci998@gmail.com>
2022-11-24 16:29:50 +0700

    add chainid euphoria-2 to swagger

--- services/api-service/smart-contracts.service.ts
+++ services/api-service/smart-contracts.service.ts
@@ -44,7 +44,7 @@ export default class SmartContractsService extends MoleculerDBService<
 	 *          required: true
 	 *          schema:
 	 *            type: string
-	 *            enum: ["aura-testnet-2","serenity-testnet-001","halo-testnet-001","theta-testnet-001","osmo-test-4","evmos_9000-4","euphoria-1","cosmoshub-4"]
+	 *            enum: ["aura-testnet-2","serenity-testnet-001","halo-testnet-001","theta-testnet-001","osmo-test-4","evmos_9000-4","euphoria-1","euphoria-2","cosmoshub-4"]
 	 *          description: "Chain Id of network need to query"
 	 *          example: "aura-testnet-2"
 	 *        - in: query
