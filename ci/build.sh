87bac843b15e71f7368c5048ef1a8772213b9965
Phan Anh Tuan <fibonacci998@gmail.com>
2022-11-24 15:06:14 +0700

    add ci

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@ set -xe
 #Login to registry
 echo $GITHUB_PASSWORD | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
 #Build and push image
-echo CONTAINER_RELEASE_IMAGE=ghcr.io/aura-nw/aura-indexer-crawl:feature-crawl-for-cosmoshub3_${SHORT_SHA_COMMIT}
+CONTAINER_RELEASE_NAME=ghcr.io/aura-nw/aura-indexer-crawl:feature-crawl-for-cosmoshub3_${SHORT_SHA_COMMIT}
 
-docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
-docker push ${CONTAINER_RELEASE_IMAGE}
+docker build -t ${CONTAINER_RELEASE_NAME} -f Dockerfile .
+docker push ${CONTAINER_RELEASE_NAME}
