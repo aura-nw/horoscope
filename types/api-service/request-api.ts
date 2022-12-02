1254b91ae9336de180531c643b329b89877e2677
phamphong9981 <phamphong9981@gmail.com>
2022-12-02 10:08:51 +0700

    add txhash query for inactive api

--- types/api-service/request-api.ts
+++ types/api-service/request-api.ts
@@ -26,9 +26,9 @@ export interface BlockHashParams {
 
 export interface GetBlockRequest
 	extends ChainIdParams,
-		PageLimit,
-		BlockHashParams,
-		BlockHeightParams {
+	PageLimit,
+	BlockHashParams,
+	BlockHeightParams {
 	operatorAddress: string;
 	consensusHexAddress: string;
 }
@@ -41,7 +41,7 @@ export interface GetVoteRequest extends ChainIdParams, PageLimit {
 export interface GetAssetByAddressRequest extends ChainIdParams, PageLimit {
 	address: string;
 }
-export interface GetAllAsset extends ChainIdParams, PageLimit {}
+export interface GetAllAsset extends ChainIdParams, PageLimit { }
 
 export interface GetAssetByOwnerAddressRequest extends ChainIdParams, PageLimit {
 	owner: string;
@@ -63,10 +63,10 @@ export interface AddressParams {
 
 export interface GetTxRequest
 	extends ChainIdParams,
-		PageLimit,
-		BlockHeightParams,
-		TxHashParams,
-		AddressParams {
+	PageLimit,
+	BlockHeightParams,
+	TxHashParams,
+	AddressParams {
 	searchType: string;
 	searchKey: string;
 	searchValue: string;
@@ -77,7 +77,7 @@ export interface GetTxRequest
 	fromHeight: number;
 	needFullLog: boolean;
 }
-export interface GetPowerEventTxRequest extends ChainIdParams, PageLimit, AddressParams {}
+export interface GetPowerEventTxRequest extends ChainIdParams, PageLimit, AddressParams { }
 
 export interface GetIBCTxRequest extends PageLimit {
 	sequenceIBC: string;
@@ -147,6 +147,7 @@ export interface GetFeegrantRequest extends ChainIdParams, PageLimit {
 export interface GetFeegrantInactiveRequest extends ChainIdParams, PageLimit {
 	granter: string;
 	grantee: string;
+	txhash: string
 }
 export interface QueryDelayJobParams {
 	address: string;
