fb066238c45d52905292b87b67e96e1c01e53349
Phan Anh Tuan <fibonacci998@gmail.com>
2022-12-12 13:56:12 +0700

    update image container name

--- ci/build.sh
+++ ci/build.sh
@@ -4,5 +4,5 @@ set -xe
 #Login to registry
 echo $GITHUB_PASSWORD | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
 #Build and push image
-docker build -t ghcr.io/aura-nw/aura-indexer-crawl:fix-eslint-1.4 -f Dockerfile .
-docker push ghcr.io/aura-nw/aura-indexer-crawl:fix-eslint-1.4
+docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
+docker push ${CONTAINER_RELEASE_IMAGE}
