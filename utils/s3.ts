4098f7a53d944196ccd57cf3b33e8fd389fa4d26
Phan Anh Tuan <fibonacci998@gmail.com>
2022-12-29 14:45:44 +0700

    fix s3 service

--- utils/s3.ts
+++ utils/s3.ts
@@ -8,18 +8,19 @@ export class S3Service {
 	private AWS_ACCESS_KEY_ID: string;
 	private AWS_SECRET_ACCESS_KEY: string;
 	private AWS_REGION: string;
-
+	private S3_CLIENT: S3;
 	public constructor() {
 		this.AWS_ACCESS_KEY_ID = Config.AWS_ACCESS_KEY_ID;
 		this.AWS_SECRET_ACCESS_KEY = Config.AWS_SECRET_ACCESS_KEY;
 		this.AWS_REGION = Config.AWS_REGION;
-	}
-
-	public connectS3() {
-		return new S3({
+		this.S3_CLIENT = new S3({
 			accessKeyId: this.AWS_ACCESS_KEY_ID,
 			region: this.AWS_REGION,
 			secretAccessKey: this.AWS_SECRET_ACCESS_KEY,
 		});
+	}
+
+	public connectS3() {
+		return this.S3_CLIENT;
 	}
 }
