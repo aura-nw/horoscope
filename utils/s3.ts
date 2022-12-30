4c5c2680d99cd3b8c170e7f7faa7ddb94c962aee
Phan Anh Tuan <fibonacci998@gmail.com>
2022-12-30 11:07:27 +0700

    change import to require in s3

--- utils/s3.ts
+++ utils/s3.ts
@@ -1,19 +1,22 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 // Import { ConfigService } from 'src/shared/services/config.service';
-import { S3 } from 'aws-sdk';
+// import { S3 } from 'aws-sdk';
+
 import { Config } from '../common';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const AWS = require('aws-sdk');
 
 export class S3Service {
 	//   Private _configService = new Config();
 	private AWS_ACCESS_KEY_ID: string;
 	private AWS_SECRET_ACCESS_KEY: string;
 	private AWS_REGION: string;
-	private S3_CLIENT: S3;
+	private S3_CLIENT: AWS.S3;
 	public constructor() {
 		this.AWS_ACCESS_KEY_ID = Config.AWS_ACCESS_KEY_ID;
 		this.AWS_SECRET_ACCESS_KEY = Config.AWS_SECRET_ACCESS_KEY;
 		this.AWS_REGION = Config.AWS_REGION;
-		this.S3_CLIENT = new S3({
+		this.S3_CLIENT = new AWS.S3({
 			accessKeyId: this.AWS_ACCESS_KEY_ID,
 			region: this.AWS_REGION,
 			secretAccessKey: this.AWS_SECRET_ACCESS_KEY,
